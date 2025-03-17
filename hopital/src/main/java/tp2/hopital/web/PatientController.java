19c50ab16e4e10dbe6c54430a482dbe7468e3995
Nisrine-C <nisrinechakir237@gmail.com>
2025-03-17 11:24:03 +0000

    Fourth Commit

--- hopital/src/main/java/tp2/hopital/web/PatientController.java
+++ hopital/src/main/java/tp2/hopital/web/PatientController.java
@@ -2,9 +2,12 @@ package tp2.hopital.web;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import tp2.hopital.entities.Patient;
 import tp2.hopital.repository.PatientRepository;
 
@@ -17,9 +20,12 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patientList = patientRepository.findAll();
-        model.addAttribute("listPatients",patientList);
+    public String index(Model model,
+                        @RequestParam(name = "p",defaultValue = "0") int page,
+                        @RequestParam(name = "s",defaultValue = "4") int size) {
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("listPatients",pagePatients.getContent());
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
         return "patients";
     }
 
